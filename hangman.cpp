bbab037d8fa1fbfcd2086a2dae9163671ce11b0f
kyledaviesdev <kylemdavies127@gmail.com>
2025-02-19 19:33:06 -0700

    got the base outline of play need to fill in all the functions that aren't used yet.

--- hangman.cpp
+++ hangman.cpp
@@ -18,15 +18,51 @@ class Hangman {
         };
 
     public:
-    Hangman() {
-        // Initialize random seed (important for different random words each time)
-        std::srand(std::time(0));  
+        Hangman() {
+            // Initialize random seed (important for different random words each time)
+            std::srand(std::time(0));  
 
-        // Choose a random word from the list
-        int randomIndex = std::rand() % wordList.size();
-        secretWord = wordList[randomIndex];
+            // Choose a random word from the list
+            int randomIndex = std::rand() % wordList.size();
+            secretWord = wordList[randomIndex];
 
-        guessedLetters.clear(); // Start with no guessed letters
-        incorrectGuesses = 0;
-    }
+            guessedLetters.clear(); // Start with no guessed letters
+            incorrectGuesses = 0;
+        }
+        void play() {
+            while (!gameOver()) {
+                displayGameState();
+                char guess = getPlayerGuess();
+                processGuess(guess);
+            }
+            displayResult();
+        }
+        private:
+            void displayGameState() {
+                std::cout << "\nSecret word: ";
+                for (char letter : secretWord) {
+                    bool found = false;
+                    for (char guessed : guessedLetters) {
+                        if (std::toupper(letter) == std::toupper(guessed)) { // Case-insensitive comparison
+                            std::cout << letter << " ";
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        std::cout << "_ ";
+                    }
+                }
+                std::cout << "\nGuessed letters: ";
+                for (char guessed : guessedLetters) {
+                    std::cout << guessed << " ";
+                }
+                std::cout << "\nIncorrect guesses: " << incorrectGuesses << "/" << maxGuesses << std::endl;
+            }
 };
+
+int main() {
+    Hangman game;
+    game.play();
+    return 0;
+}
